2c5046e9e309be7f5a98cb5f4cd42b2456e6690d
2code <2mail2code@gmail.com>
2024-04-08 05:04:12 +0200

    update CsvReaderOfT interface to expose Error event

--- src/Code2.Tools.Csv/ICsvReaderOfT.cs
+++ src/Code2.Tools.Csv/ICsvReaderOfT.cs
@@ -5,6 +5,7 @@ namespace Code2.Tools.Csv
 	public interface ICsvReader<T> : ICsvReader
 		where T : class
 	{
+		event EventHandler<UnhandledExceptionEventArgs>? Error;
 		Func<string[], int, T> Deserializer { get; set; }
 
 		T ReadObject();
